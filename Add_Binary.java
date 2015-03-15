a305c41cadc3150d21cef2c62d2f215260dc14dc
SuryaTeja Duggi <suryateja008@gmail.com>
2015-03-15 15:49:59 -0700

    Add Binary Solution LeetCode

--- Add_Binary.java
+++ Add_Binary.java
@@ -1,22 +1,23 @@
 public class Solution {
     public String addBinary(String a, String b) {
         
-        int carry =0;
-        String res="";
+   StringBuffer ss = new StringBuffer(); 
+	int carry =0;
 	int alen = a.length();
 	int blen = b.length();
 	
 	for(int i=alen-1, j=blen-1;i>=0||j>=0;i--,j--){
-		int aval = (i>=0)? a.charAt(i)-'0' :0;
+		int aval = (i>=0)? a.charAt(i)-'0' : 0;
 		int bval = (j>=0)? b.charAt(j)-'0' :0;
 		int val = (aval+bval+carry)%2;
 		carry = (aval+bval+carry)/2;
-		res=val+res;
+		ss.insert(0,val);
+		
 	}
 	
 	   if(carry == 1)
-		   res=carry+res;
+		   ss.insert(0,carry);
 		   
-    return res;
+    return ss.toString();
     }
 }
