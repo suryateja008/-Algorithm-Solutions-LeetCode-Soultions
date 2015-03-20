74a5423e3a79bdce74564433c0773d4a7b6cb7b6
SuryaTeja Duggi <suryateja008@gmail.com>
2015-03-20 03:22:36 -0700

    Length Of Last Word Java LeetCode Solution

---
+++ Length_Of_Last_Word.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        int c=0;
+        s=s.trim();
+		for(int i=s.length()-1;i>=0;i--){
+			if(s.charAt(i)!=' ')
+			{
+				c++;
+			}
+			else
+			{
+				break;
+			}
+		}
+		
+		return c;
+    }
+}
