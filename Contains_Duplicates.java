ffa226427e038fa70b1f03f3eae1f521ad145eed
SuryaTeja Duggi <suryateja008@gmail.com>
2015-06-07 19:46:55 -0700

    Contains Duplicate

---
+++ Contains_Duplicates.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashMap<Integer,Integer> h = new HashMap<Integer,Integer>();
+	
+	for(int i=0;i<nums.length;i++)
+	{
+		if(h.containsKey(nums[i]))
+		    	return true;
+		else
+			h.put(nums[i],i);
+	}
+	
+	
+	
+	return false;
+    }
+}
